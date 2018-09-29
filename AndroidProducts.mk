a700151e31912aa9955225059af647126958ffda
Ali B <abittin@gmail.com>
2018-09-29 12:57:15 +0300

    dumpling: aicpfy
    Change-Id: I0beff9d477712c94299625d4f438ea3c6f776d80

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2018 AICP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,4 +16,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/aicp_dumpling.mk
